2a6cd9c083c416cad78c61761b243dbd94165ba5
shadman-ahmed-bs23 <shadmanahmed874@gmail.com>
2020-10-19 21:44:06 +0600

    Reducers added and post lists rendered

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import jsonplaceholder from '../apis/jsonplaceholder';
 export const fetchPosts =  () => async dispatch => {
   const response = await jsonplaceholder.get('/posts'); 
 
-  dispatch({ type: 'FETCH_POSTS', payload: response })
+  dispatch({ type: 'FETCH_POSTS', payload: response.data })
 }; 
 
 
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -6,13 +6,36 @@ class PostList extends React.Component {
   componentDidMount() {
     this.props.fetchPosts(); 
   }
+
+  renderList() {
+    console.log(this.props.posts);
+    return this.props.posts.map((post) => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user"/>
+          <div className="content">
+            <div className="description">
+              <h2>{ post.title }</h2>
+              <p>{ post.body }</p>
+            </div>
+          </div>
+        </div>
+      ); 
+    });
+     
+  }
   render() {
+    console.log(this.props.posts); 
     return(
-      <div>
-        <h1>PostList</h1>
+      <div className="ui relaxed divided list">
+        {this.renderList()}
       </div>
     ); 
   }
 }
 
-export default connect(null, {fetchPosts: fetchPosts})(PostList);
+const mapStateToProps = (state) => {
+  return {posts: state.posts}; 
+}
+
+export default connect(mapStateToProps, {fetchPosts: fetchPosts})(PostList);
